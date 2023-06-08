218be94a179baeddfcb0f75cc6eafe20fc4f0b8b
vanh0210 <135860457+vanh0210@users.noreply.github.com>
2023-06-08 17:15:09 +0700

    Update prescribe.php

--- prescribe.php
+++ prescribe.php
@@ -122,18 +122,18 @@ if(isset($_POST['prescribe']) && isset($_POST['pid']) && isset($_POST['ID']) && 
                   <div class="col-md-4"><label>Disease:</label></div>
                   <div class="col-md-8">
                   <!-- <input type="text" class="form-control" name="disease" required> -->
-                  <textarea id="disease" cols="86" rows ="5" name="disease" required></textarea>
+                  <textarea id="disease" cols="76" rows ="3" name="disease" required></textarea>
                   </div><br><br><br>
                   
                   <div class="col-md-4"><label>Allergies:</label></div>
                   <div class="col-md-8">
                   <!-- <input type="text"  class="form-control" name="allergy" required> -->
-                  <textarea id="allergy" cols="86" rows ="5" name="allergy" required></textarea>
+                  <textarea id="allergy" cols="76" rows ="3" name="allergy" required></textarea>
                   </div><br><br><br>
                   <div class="col-md-4"><label>Prescription:</label></div>
                   <div class="col-md-8">
                   <!-- <input type="text" class="form-control"  name="prescription"  required> -->
-                  <textarea id="prescription" cols="86" rows ="10" name="prescription" required></textarea>
+                  <textarea id="prescription" cols="76" rows ="5" name="prescription" required></textarea>
                   </div><br><br><br>
                   <input type="hidden" name="fname" value="<?php echo $fname ?>" />
                   <input type="hidden" name="lname" value="<?php echo $lname ?>" />
